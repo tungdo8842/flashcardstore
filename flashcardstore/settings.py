3890eaa0d406721d1956f936edd76b90d6e85b0c
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2026-07-09 16:29:14 -0700

    Add CORS header

--- flashcardstore/settings.py
+++ flashcardstore/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'api',
     'rest_framework',
+    'corsheaders',
 ]
 
 # REST_FRAMEWORK = {
@@ -52,6 +53,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+]
